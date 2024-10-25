7dfe032bb7f1a5fc109558a4c69b20441f244444
Jaxon Adams <jadamsresume1@gmail.com>
2024-10-25 17:08:54 -0600

    Add replacements for Flask default errors
    When testing, I noticed that Flask's default error message bodies (for
    status code 404 or 500 requests, for example) are in HTML format. This
    doesn't mesh well with my usual JSON response bodies, so these functions
    replace the default error message bodies.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from routes import Router
 
@@ -11,6 +11,37 @@ router.init()
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+# TODO: move error methods to a place that makes more sense
+@app.errorhandler(401)
+def unauthorized_error(error):
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error):
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not Found"}), 404
+
+
+@app.errorhandler(409)
+def invalid_request(error):
+    return jsonify({"error": "Invalid or Malformed Request"}), 409
+
+
+@app.errorhandler(415)
+def media_not_supported_error(error):
+    return jsonify({"error": "Unsupported Media Type"}), 415
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({"error": "Internal Server Error"}), 500
 
 
 if __name__ == "__main__":
