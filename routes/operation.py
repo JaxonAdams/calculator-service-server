41173d59b74fcf2b3dd18f88a8f4efcc3b96fb64
Jaxon Adams <jaxon.adams@loanpro.io>
2024-10-30 10:00:26 -0600

    Implement route for getting operation by ID
    This route causes previously defined unit tests to pass.

--- routes/operation.py
+++ routes/operation.py
@@ -15,3 +15,18 @@ def manage_operations():
         ops = db.fetch_records("operation")
 
     return jsonify({"results": ops}), 200
+
+
+@operation_bp.route("/<int:operation_id>", methods=["GET"])
+@jwt_required
+def manage_single_operation(operation_id):
+    with DBService() as db:
+        ops = db.fetch_records(
+            "operation",
+            conditions={"id": operation_id},
+        )
+
+    try:
+        return jsonify(ops[0]), 200
+    except IndexError:
+        return jsonify({"error": f"Operation with ID {operation_id} not found"}), 404
